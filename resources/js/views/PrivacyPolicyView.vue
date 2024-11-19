b16fc848583c9261b5159d228f085f913cc662b2
Hemlock4414 <antonio.marrara@gmx.ch>
2024-11-19 09:06:16 +0100

    update

--- resources/js/views/PrivacyPolicyView.vue
+++ resources/js/views/PrivacyPolicyView.vue
@@ -7,105 +7,112 @@
 <div class="container">
     <div class="page-content">
 
-        <h1>Datenschutzerklärung</h1>
+        <h1 class="heading">Datenschutzerklärung</h1>
 
         <p>
         "NAME" will Ihre persönlichen Daten schützen und mit der vorliegenden Datenschutzerklärung 
         ("Erklärung") zeigen wir Ihnen auf, in welcher Weise wir Ihre Personendaten verwenden und wie 
-        "NAME" (nachstehend als „die Webseite“ oder auch wir/uns bezeichnet) diese schützt.
-
+        "NAME" (nachstehend als „die Webseite“ oder auch wir/uns bezeichnet) diese schützt.</p><br>
+        <p>
         Personendaten im Sinn dieser Erklärung sind alle Daten, die wir im Zusammenhang mit Ihrer Nutzung 
         von dieser Webseite erhalten und anhand derer Sie direkt oder indirekt identifiziert werden können, wie 
-        beispielsweise Name, Adresse, Geburtsdatum und E-Mail-Adresse.
-
+        beispielsweise Name, Adresse, Geburtsdatum und E-Mail-Adresse.</p><br>
+        <p>
         Wir erwarten, dass Sie diese Erklärung lesen, bevor Sie diese Webseite nutzen. Mit Ihrer Nutzung von 
-        dieser Webseite akzeptieren Sie diese Erklärung.
-        </p>
+        dieser Webseite akzeptieren Sie diese Erklärung.</p><br>
+        
+        <section>
+            <h2 class="heading">Welche Art von personenbezogenen Daten erheben und bearbeiten wir?</h2>
 
+            <p>Abhängig von Ihren Aktivitäten und den auf der Website/in den Apps ausgewählten Diensten 
+            verarbeiten wir die folgenden Daten vollständig oder teilweise.</p><br>
+            <p>
+            Die von Ihnen bei der Einrichtung Ihres Spielerkontos erhaltenen Daten, insbesondere Name, 
+            Geburtsdatum, Nationalität, E-Mail-Adresse und Passwort.</p>
+            <p>
+            Die bei Ihrer Nutzung von unserer Webseite erzeugten Aktivitätsdaten, insbesondere Gerätedaten 
+            inklusive IP-Adresse, Quell- und Zieldaten, Spieler-ID, Spieltransaktionen, 
+            Website-Login-Verlauf, Analyseinformationen.</p><br>
+            <p>
+            Daten, die mithilfe von Cookies während Ihrer Nutzung von unserer Webseite gesammelt werden und 
+            nicht gänzlich anonym erfasst werden, sondern mit Personendaten in Verbindung stehen oder 
+            verknüpft sind. Weitere Informationen über Cookies und deren Verwaltung finden Sie in 
+            unseren <a href="/agb#C13">Cookie-Richtlinien</a>.
+            </p><br>
+        </section>
+        
+        <section>
+            <h2 class="heading">Für welche Zwecke werden Ihre Personendaten bearbeitet?</h2>
 
-        <h2>Welche Art von personenbezogenen Daten erheben und bearbeiten wir?</h2>
+            <p>Wir verarbeiten Ihre Daten für die folgenden Zwecke:</p><br>
 
-        <p>Abhängig von Ihren Aktivitäten und den auf der Website/in den Apps ausgewählten Diensten 
-        verarbeiten wir die folgenden Daten vollständig oder teilweise.
+            <ul>
+                
+                <li><p>Betrieb und Verwaltung von dieser Webseite, inklusive Einrichtung und Führung Ihres Spielerkontos.</p></li>
+                
+                <li><p>Erfüllung unserer vertraglichen Verpflichtungen gegenüber den Spielern gemäss den Vorgaben 
+                der Gesetze und der Allgemeinen Geschäftsbedingungen (AGB).</p></li>
+                
+                <li><p>Verbesserung der Sicherheit, der Dienste und der Funktionen von dieser Webseite. Dies kann 
+                Recherchen, Umfragen, Ersuchen um Ihr freiwilliges Feedback, interne Schulungen sowie 
+                Dienstleistungen von Partnern beinhalten.</p></li>
+            </ul><br>
+        </section>
 
-    Die von Ihnen bei der Einrichtung Ihres Spielerkontos erhaltenen Daten, insbesondere Name, 
-    Geburtsdatum, Nationalität, E-Mail-Adresse und Passwort;
-    Die bei Ihrer Nutzung von StarVegas.ch erzeugten Aktivitätsdaten, insbesondere Gerätedaten 
-    inklusive IP-Adresse, Quell- und Zieldaten, Spieler-ID, Spieltransaktionen, 
-    Website-Login-Verlauf, Analyseinformationen;
-    Daten, die mithilfe von Cookies während Ihrer Nutzung von StarVegas.ch gesammelt werden und 
-    nicht gänzlich anonym erfasst werden, sondern mit Personendaten in Verbindung stehen oder 
-    verknüpft sind. Weitere Informationen über Cookies und deren Verwaltung finden Sie in 
-    unserer Cookie-Richtlinie.
-</p>
+        <section>
+            <h2 class="heading">Datenweitergabe</h2>
 
-    <h2>Für welche Zwecke werden Ihre Personendaten bearbeitet?</h2>
+            <p>Ihre Daten werden nicht an Dritte weitergegeben.</p> <br>    
+            <p>
+            Bitte bedenken Sie, dass Daten, die Sie mit gewissen grossen ausländischen Verarbeitern teilen 
+            (z. B. Google, Meta/Facebook, X/Twitter, Apple, Microsoft) besonderen Regeln und Risiken 
+            unterliegen.</p><br>
+            <p>
+            Wir nutzen hauptsächlich Rechenzentren in der Schweiz für die Verarbeitung 
+            personenbezogener Daten.</p><br>
+        </section>
 
-    <p>Wir verarbeiten Ihre Daten für die folgenden Zwecke:
+        <section>
+            <h2 class="heading">Dauer der Aufbewahrung von Personendaten</h2>
 
-    Betrieb und Verwaltung von dieser Webseite, inklusive Einrichtung und Führung Ihres Spielerkontos;
+            <p>Wir verarbeiten und speichern Ihre Personendaten, solange es für die Erfüllung unserer 
+            vertraglichen und gesetzlichen Pflichten oder sonst die mit der Bearbeitung verfolgten Zwecke 
+            erforderlich ist, d.h. also zum Beispiel für die Dauer der gesamten Geschäftsbeziehung sowie 
+            darüber hinaus gemäss den gesetzlichen Aufbewahrungs- und Dokumentationspflichten. Dabei ist es 
+            möglich, dass Personendaten für die Zeit aufbewahrt werden, in der Ansprüche gegen unser 
+            Unternehmen geltend gemacht werden können (d.h. insbesondere während der gesetzlichen 
+            Verjährungsfrist) und soweit wir anderweitig gesetzlich dazu verpflichtet sind oder berechtigte 
+            Geschäftsinteressen dies erfordern (z.B. für Beweis- und Dokumentationszwecke). Sobald Ihre 
+            Personendaten für die in Kapitel 4 genannten Zwecke nicht mehr erforderlich sind, werden sie 
+            grundsätzlich und soweit möglich gelöscht oder anonymisiert. Für betriebliche Daten (z.B. 
+            Systemprotokolle, Logs) gelten grundsätzlich kürzere Aufbewahrungsfristen von zwölf Monaten oder 
+            weniger.</p><br>
+        </section>
 
-    Erfüllung unserer vertraglichen Verpflichtungen gegenüber den Spielern gemäss den Vorgaben 
-    der Gesetze und der Allgemeinen Geschäftsbedingungen (AGB);
+        <section>
+            <h2 class="heading">Kontaktformulare</h2>
 
-    Verbesserung der Sicherheit, der Dienste und der Funktionen von dieser Webseite. Dies kann 
-    Recherchen, Umfragen, Ersuchen um Ihr freiwilliges Feedback, interne Schulungen sowie 
-    Dienstleistungen von Partnern beinhalten;
-</p>
+            <p>Unsere Webseite enthält Kontaktformulare, die genutzt werden können, um mit uns Kontakt 
+            aufzunehmen. Die in die Kontaktformulare eingegebenen Daten werden für 
+            Zwecke der Bearbeitung Ihrer Anfrage oder der Kontaktaufnahme zur kontaktaufnehmenden Person 
+            gespeichert. Die Nutzung unserer Kontaktformulare erfolgt auf ausdrücklich freiwilliger Basis und 
+            Ihrer Einwilligung.
+            </p><br>
+        </section>
 
+        <section>  
+            <h2 class="heading">Cookies</h2>
 
-    <h2>Datenweitergabe</h2>
-<p>Daten werden nicht weitergegeben an Dritte
+            <p>Wir setzen auf unserer Website Cookies ein. Wir haben separate <a href="/agb#C13">Cookie-Richtlinien</a> festgelegt, die 
+            ein integraler Bestandteil der vorliegenden Datenschutzerklärung sind.</p><br>
+            
+            <h2 class="heading">Änderungen dieser Erklärung</h2>
 
-Bitte bedenken Sie, dass Daten, die Sie mit gewissen grossen ausländischen Verarbeitern teilen 
-(z. B. Google, Meta/Facebook, X/Twitter, Apple, Microsoft) besonderen Regeln und Risiken 
-unterliegen.
+            <p>Diese Erklärung kann durch uns jederzeit geändert werden, weshalb wir Sie bitten, diese 
+            Erklärung regelmässig zu konsultieren. Es gilt die jeweils auf der Webseite publizierte Fassung.</p><br>
 
-Die Casino Interlaken AG nutzt hauptsächlich Rechenzentren in der Schweiz für die Verarbeitung 
-personenbezogener Daten.
-</p>
-
-<h2>Dauer der Aufbewahrung von Personendaten</h2>
-
-<p>Wir verarbeiten und speichern Ihre Personendaten, solange es für die Erfüllung unserer 
-vertraglichen und gesetzlichen Pflichten oder sonst die mit der Bearbeitung verfolgten Zwecke 
-erforderlich ist, d.h. also zum Beispiel für die Dauer der gesamten Geschäftsbeziehung sowie 
-darüber hinaus gemäss den gesetzlichen Aufbewahrungs- und Dokumentationspflichten. Dabei ist es 
-möglich, dass Personendaten für die Zeit aufbewahrt werden, in der Ansprüche gegen unser 
-Unternehmen geltend gemacht werden können (d.h. insbesondere während der gesetzlichen 
-Verjährungsfrist) und soweit wir anderweitig gesetzlich dazu verpflichtet sind oder berechtigte 
-Geschäftsinteressen dies erfordern (z.B. für Beweis- und Dokumentationszwecke). Sobald Ihre 
-Personendaten für die in Kapitel 4 genannten Zwecke nicht mehr erforderlich sind, werden sie 
-grundsätzlich und soweit möglich gelöscht oder anonymisiert. Für betriebliche Daten (z.B. 
-Systemprotokolle, Logs) gelten grundsätzlich kürzere Aufbewahrungsfristen von zwölf Monaten oder 
-weniger.
-</p>
-
-<h2>6.2 Kontaktformulare</h2>
-
-<p>Unsere Webseite enthält Kontaktformulare, die genutzt werden können, um mit uns Kontakt 
-aufzunehmen. Die Übertragung der in den Kontaktformularen eingegebenen Daten erfolgt 
-verschlüsselt (SSL Verschlüsselung). Die in die Kontaktformulare eingegebenen Daten werden für 
-Zwecke der Bearbeitung Ihrer Anfrage oder der Kontaktaufnahme zur kontaktaufnehmenden Person 
-gespeichert. Die Nutzung unserer Kontaktformulare erfolgt auf ausdrücklich freiwilliger Basis und 
-Ihrer Einwilligung.
-</p>
-
-<h2>Cookies</h2>
-
-<p>Wir setzen auf unserer Website Cookies ein. Wir haben separate Cookie-Richtlinien festgelegt, die 
-ein integraler Bestandteil der vorliegenden Datenschutzerklärung sind.
-</p>
-
-
-
-<h2>Änderungen dieser Erklärung</h2>
-
-<p>Diese Erklärung kann durch StarVegas.ch jederzeit geändert werden, weshalb wir Sie bitten, diese 
-Erklärung regelmässig zu konsultieren. Es gilt die jeweils auf der Website publizierte Fassung.
-
-Derzeitiger Stand ist Dezember 2024. 
-</p>
+            <p>Derzeitiger Stand ist Dezember 2024.</p>
+        </section>      
 
     </div>    
 </div>
@@ -127,5 +134,10 @@ Derzeitiger Stand ist Dezember 2024.
     margin-right: 0;
     display: flex;
     flex-direction: column;
+}
+
+.heading {
+    margin-top: 10px;
+    margin-bottom: 5px;
 }
 </style>
