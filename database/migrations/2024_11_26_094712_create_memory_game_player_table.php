4e32f9b006d1a1e10530733e9cf8e0bdd86f06c2
Hemlock4414 <antonio.marrara@gmx.ch>
2024-12-01 17:31:48 +0100

    update

--- database/migrations/2024_11_26_094712_create_memory_game_player_table.php
+++ database/migrations/2024_11_26_094712_create_memory_game_player_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('memory_game_players');
+        Schema::dropIfExists('memory_game_player');
     }
 };
