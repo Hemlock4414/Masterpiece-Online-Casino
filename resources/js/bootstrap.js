5f78228454ae5cf72e059a2de61687dbdbf34953
Hemlock4414 <antonio.marrara@gmx.ch>
2024-12-03 11:17:25 +0100

    update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
+import Echo from 'laravel-echo'
 
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
+});
