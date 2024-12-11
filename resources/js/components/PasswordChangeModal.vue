cad1b7f63bc79d3dce4dd395abbb8170571c3292
Hemlock4414 <antonio.marrara@gmx.ch>
2024-12-11 14:11:22 +0100

    update

--- resources/js/components/PasswordChangeModal.vue
+++ resources/js/components/PasswordChangeModal.vue
@@ -31,7 +31,7 @@ const handleSubmit = async () => {
       password_confirmation: confirmPassword.value
     });
     
-    emit('success', 'Passwort wurde erfolgreich geändert. Sie werden nun ausgeloggt...');
+    emit('success', 'Passwort wurde erfolgreich geändert. Aus Sicherheitsgründen werden Sie nun ausgeloggt. Bitte benutzen Sie sich anschliessend Ihr neues Passwort.');
     setTimeout(() => {
       router.push('/');
     }, 2000);
